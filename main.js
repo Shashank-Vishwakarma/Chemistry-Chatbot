b7f7b27a2b714dbe6331808be8e0b540c746718c
shashank <shashank78680@gmail.com>
2024-01-26 00:00:16 +0530

    initialized chatbot project

---
+++ main.js
@@ -0,0 +1,24 @@
+import { app, BrowserWindow } from "electron";
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 1000,
+        height: 700,
+    });
+
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(()=>{
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+}).catch((error)=>{
+    console.log("error : ", error)
+})
+
+app.on("window-all-closed", ()=>{
+    if(process.platform !== 'darwin') app.quit();
+})
