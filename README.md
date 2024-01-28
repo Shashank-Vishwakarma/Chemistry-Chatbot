354d992b7a89aacd26d4986f61ebaff0e44c66ae
shashank <shashank78680@gmail.com>
2024-01-29 00:06:25 +0530

    post api is ready

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Chemistry-Chatbot
 Tech: HTML, CSS, JavaScript, ElectronJs
+
+# For backend (inside chemcrow-backend)
+### Create an python environment.
+```
+python -m venv .venv
+
+source .venv/Scripts/activate
+```
+
+### Install the required packages
+```
+pip install -r requirements.txt
+```
--- src/renderer/js/script.js
+++ src/renderer/js/script.js
@@ -2,8 +2,28 @@ const inputBox = document.querySelector(".search input");
 const sendButton = document.querySelector(".search button");
 const answerBox = document.querySelector(".chat-box");
 
+async function getResponse() {
+    const response = await fetch('http://localhost:5000/api/post', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    const data = await response.text();
+    return data;
+}
+
 sendButton.addEventListener("click", ()=>{
     const question = inputBox.value;
-    answerBox.innerHTML = question;
-    inputBox.value = "";
+    // get the response
+    const answer = getResponse();
+
+    answerBox.innerHTML = "Please wait a few seconds for the answer...";
+    answer.then((data)=>{
+        answerBox.innerHTML = data;
+        inputBox.value = "";
+    }).catch((error)=>{
+        answerBox.innerHTML = error;
+        inputBox.value = "";
+    })
 })
